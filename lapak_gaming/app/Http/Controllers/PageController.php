af5f5225eb0b714e7962dc02d403034561d94c7b
Muhammad Dzakwan Ismail Rangkuti <zakwanrangkuti9@gmail.com>
2026-05-10 16:21:41 +0700

    Update

--- lapak_gaming/app/Http/Controllers/PageController.php
+++ lapak_gaming/app/Http/Controllers/PageController.php
@@ -4,11 +4,18 @@ namespace App\Http\Controllers;
 
 class PageController extends Controller
 {
-    public function contact() {
+    public function contact()
+    {
         return view('pages.contact');
     }
 
-    public function terms() {
+    public function terms()
+    {
         return view('pages.terms');
+    }
+
+    public function privacy()
+    {
+        return view('pages.privacy');
     }
 }
