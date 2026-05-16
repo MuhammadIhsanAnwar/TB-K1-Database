ed8297246369e82bfbcd75d26f7b7c40cea60bd3
Novri Ilyasah <nopriilyasah2506@gmail.com>
2026-05-16 11:38:33 +0700

    update

--- lapak_gaming/resources/views/seller/products/index.blade.php
+++ lapak_gaming/resources/views/seller/products/index.blade.php
@@ -38,7 +38,7 @@ use Illuminate\Support\Facades\Storage;
             @forelse($products as $product)
                 <div class="group rounded-[2.5rem] border border-slate-800 bg-slate-900 p-8 transition-all hover:border-amber-500/30">
                     <div class="flex flex-col gap-6 md:flex-row md:items-center">
-                        <img src="{{ Storage::url($product->image) }}"
+                        <img src="{{ Storage::url($product->file_path) }}"
                              class="w-24 h-24 rounded-3xl object-cover border-2 border-slate-800 group-hover:border-amber-500/50 transition-all">
                         
                         <div class="flex-1">
