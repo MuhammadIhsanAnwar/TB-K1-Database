34b3896375380c6a30c8fb843a5e3ec27c185c00
Muhammad Dzakwan Ismail Rangkuti <zakwanrangkuti9@gmail.com>
2026-05-19 16:02:44 +0700

    Update

--- lapak_gaming/resources/views/orders/show.blade.php
+++ lapak_gaming/resources/views/orders/show.blade.php
@@ -229,12 +229,12 @@
 
                     <div class="mt-5 overflow-hidden rounded-[24px] border border-white/10">
 
-                        <a href="{{ asset('storage/'. $order->payment_proof) }}"
+                        <a href="{{ route('orders.viewProof', basename($order->payment_proof)) }}"
                            target="_blank"
                            rel="noopener noreferrer">
 
                             <img
-                                src="{{ asset('storage/' . $order->payment_proof) }}"
+                                src="{{ route('orders.viewProof', basename($order->payment_proof)) }}"
                                 alt="Bukti Pembayaran"
                                 class="w-full object-cover transition duration-300 hover:scale-[1.01] max-h-[500px]"
                             />
